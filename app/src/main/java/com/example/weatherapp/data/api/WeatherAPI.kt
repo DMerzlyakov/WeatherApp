af44235967fde6561300e745bcf09c50e47bd1b4
Merzlyakov Danila <merzlyakovdanila@gmail.com>
2022-11-17 01:05:31 +0300

    Готовое приложение

--- app/src/main/java/com/example/weatherapp/data/api/WeatherAPI.kt
+++ app/src/main/java/com/example/weatherapp/data/api/WeatherAPI.kt
@@ -1,5 +1,7 @@
 package com.example.weatherapp.data.api
 
+import com.example.weatherapp.common.Constants.API_KEY
+import com.example.weatherapp.common.Constants.CURRENT_WEATHER_LINK
 import com.example.weatherapp.data.api.dto.currentWeather.CurrentWeatherDTO
 import com.example.weatherapp.data.api.dto.forecastWeatherDaily.ForecastDT
 import retrofit2.Call
@@ -9,19 +11,19 @@ import retrofit2.http.Query
 // Интерфейс для реализации запросов по адресу
 interface WeatherAPI {
 
-    @GET("current?key=fd5991c796194742b5f0a663c468dac2&lang=ru")
+    @GET("${CURRENT_WEATHER_LINK}key=$API_KEY&lang=ru")
     fun getCurrentWeather(@Query("city") city: String): Call<CurrentWeatherDTO>
 
-    @GET("current?key=fd5991c796194742b5f0a663c468dac2&lang=ru")
+    @GET("${CURRENT_WEATHER_LINK}key=$API_KEY&lang=ru")
     fun getCurrentWeather(
         @Query("lat") lat: String,
         @Query("lon") lon: String
     ): Call<CurrentWeatherDTO>
 
-    @GET("forecast/daily?key=fd5991c796194742b5f0a663c468dac2&lang=ru&days=8")
+    @GET("forecast/daily?key=$API_KEY&lang=ru&days=8")
     fun getForecastWeather(@Query("city") city: String): Call<ForecastDT>
 
-    @GET("forecast/daily?key=fd5991c796194742b5f0a663c468dac2&lang=ru&days=8")
+    @GET("forecast/daily?key=$API_KEY&lang=ru&days=8")
     fun getForecastWeather(@Query("lat") lat: String, @Query("lon") lon: String): Call<ForecastDT>
 }
 
